6fd82c5c8914dbedb1f72a53800dd77ea88f5645
danielgstyles <daniel.g.styles@det.nsw.edu.au>
2024-03-17 16:06:33 +1100

    Added Questions from External File Example

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ side3 = int(input("Side 3 Size"))
 file1 = open("Sides.txt", "r+") 
 print(file1.read())
 
-file1.write(str(side1) + ", " + str(side2) +"\n")
+file1.write(str(side1) + ", " + str(side2) + str(side3) +"\n")
 file1.close()
 
 file1 = open("Sides.txt", 'r')
--- ReadQuestions.py
+++ ReadQuestions.py
@@ -1,21 +1,57 @@
 file1 = open("Questions.txt", "r+") 
 list = file1.readlines()
-print(list)
+file1.close
 
-print(list[0])
-question = list[0]
-question = question.split(':')
-print(question)
-len = len(list)
+def AddQuestion():
+    file1 = open("Questions.txt", "r+")
+    Question = input("Please enter your question. \n")
+    Answer = input("Please enter the answer to your question. \n")
 
-#print(len)
-i = 0
-while i < len:
-    question = list[i]
-    question = question.split(':')
+    print("The question and answer you entered in:  Q: " , Question , "  A:  " , Answer)
+    print("\n")
+    MenuChoice1 = input("Is this question and answer correct?  (Y/N)")
+    MenuChoice1 = MenuChoice1.upper()
+
+    if MenuChoice1 == "Y":
+        MenuChoice2 = input("Do you wich top save your new question to the Questions file?")
+        MenuChoice2 = MenuChoice2.upper()
+        if MenuChoice2 == "Y":
+            file1 = open("Questions.txt", "a") 
+            file1.write(Answer + ":" + Question + "\n")
+            file1.close
+    else:
+        print("Please try again.")
+
+
+def AskingQuestions(SubList, SubLen):
+    score = 0
+    for i in range(0,SubLen):
+        question = SubList[i]
+        question = question.split(':')
+        #print(i)
+
+        UserAnswer = input(question[1] + "? ")
+        UserAnswer = UserAnswer.upper()
+        #print(UserAnswer)
+
+        ProgAnswer = question[0]
+        ProgAnswer = ProgAnswer.upper()
+        #print(ProgAnswer)
+
+        if UserAnswer == ProgAnswer:
+            print("Correct")
+            score = score + 1
+        else:
+            print("Incorrect")
+
     #print(i)
-    answer = input(question[1] + " ")
-    if answer == question[0]:
-        print("Correct")
-    i =i+1
-    print(i)
+    print(score)
+
+Len = len(list)
+
+
+#Main Program - Menu Menu
+AddQuestion()
+#AskingQuestions(list, Len)
+
+
