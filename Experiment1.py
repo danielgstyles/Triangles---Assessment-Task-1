cdf437d6e911c767eb5333f5b160de9143b5ee49
danielgstyles <daniel.g.styles@det.nsw.edu.au>
2024-03-20 12:41:40 +1100

    Added Experiment File

---
+++ Experiment1.py
@@ -0,0 +1,17 @@
+#storing data in multiple variables
+#side1 = input("Side 1: ")
+#side2 = input("Side 2: ")
+#side3 = input("Side 3: ")
+
+#Using Lists
+name = []
+name = ["Bob", "Harry", "John"]
+print(name)
+name[1] = "Sally"
+print(name)
+#print(name[1])
+name.append("Sam")
+print(name)
+
+
+
